309ab071cdbbf1fab51835ae449f8942cf49aa21
Isaac Hill <iwhill@outlook.com>
2022-08-18 00:59:52 -0700

    Add calculator logic and display functions
    Added function 'calculator' which serves as a callback function when
    adding event listeners to all divs of class 'button'.
    
    'Calculator' serves as a logical roadmap that directs the application to
    the appropriate functions to call depending on where the user is
    currently at in their calculation.
    
    Function setFirstOperand, like the name says, sets the first operand in
    the expression depending on what number the user has entered on the
    calculator.
    
    Function setOperator sets the operator depending on what operator the
    user picked. Also evaluates the previous operand-operator pair if user
    entered one before choosing another operator (for example, if user
    enters 2 + 2, and then presses another operator button like +, -, etc.
    setOperator will evaulate 2 + 2, and 4 will be the new number that the
    operator is operating on).
    
    Function setSecondOperand sets the second operand in the expression
    depending on what number the user has entered on the calculator.
    
    Function updateDisplay changes the display whenever the user enters a
    number or evaluates a operand-operator pair.
    
    Function resetCalc resets the state of the calculator, wiping all
    operator and operand data. This is called when clicking the clear (AC)
    button or clicking a number after evaluating an operand-operator pair
    with the = button.
    
    Function resetDisplay is called alongside resetCalc and sets the display
    to read 0.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function add (a, b) {
-  return parseInt(a) + parseInt(b);
+  return (parseInt(a) + parseInt(b)).toString();
 }
 
 function subtract(a, b) {
@@ -11,21 +11,122 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (b === "0") {
+    return 'ERROR!';
+  }
   return a / b;
 }
 
 function operate(a, b, operator) {
   let result;
 
-  if (operator === "add") {
+  if (operator === "+") {
     result = add(a, b);
-  } else if (operator ==="subtract") {
+  } else if (operator ==="-") {
     result = subtract(a, b);
-  } else if (operator === "multiply") {
+  } else if (operator === "x") {
     result = multiply(a, b);
-  } else if (operator === "divide") {
+  } else if (operator === "/") {
     result = divide(a, b)
   }
 
   return result;
 }
+
+function updateDisplay(str) {
+  const display = document.querySelector('.display-content');
+
+  display.textContent = str;
+}
+
+function setFirstOperand(selection) {
+  if (calcState.x === undefined) {
+    calcState.x = selection;
+  } else {
+    calcState.x += selection;
+  }
+
+  updateDisplay(calcState.x);
+}
+
+function setOperator(selection) {
+  calcState.ans = undefined;
+
+  if (calcState.opr !== undefined && calcState.y !== undefined) {
+    calcState.x = operate(calcState.x, calcState.y, calcState.opr);
+    updateDisplay(calcState.x);
+    calcState.y = undefined;
+    calcState.opr = selection;
+  } else if (calcState.opr === undefined && calcState.x !== undefined) {
+    calcState.opr = selection;
+  } else if (calcState.opr !== undefined) {
+    calcState.opr = selection;
+  }
+}
+
+function setSecondOperand(selection) {
+  if (calcState.y === undefined) {
+    calcState.y = selection;
+  } else {
+    calcState.y += selection;
+  }
+
+  updateDisplay(calcState.y);
+}
+
+function resetCalc() {
+  calcState.x = undefined;
+  calcState.y = undefined;
+  calcState.opr = undefined;
+  calcState.ans = undefined;
+}
+
+function resetDisplay() {
+  const display = document.querySelector('.display-content');
+
+  display.textContent = 0;
+}
+
+
+
+function calculate(event) {
+  const button = event.target.textContent;
+
+  if (button >= "0" && button <= "9" && calcState.ans !== undefined) {
+    resetCalc();
+  }
+
+  if (button >= "0" && button <= "9" && calcState.opr === undefined) {
+    setFirstOperand(button);
+  }
+
+  if (button === "+" || button === "-" || button === "x" || button === "/") {
+    setOperator(button);
+  }
+
+  if (button >= "0" && button <= "9" && calcState.opr !== undefined) {
+    setSecondOperand(button);
+  }
+
+  if (button === "=") {
+    calcState.ans = operate(calcState.x, calcState.y, calcState.opr);
+    updateDisplay(calcState.ans);
+  }
+
+  if (button === "AC") {
+    resetDisplay();
+    resetCalc();
+  }
+}
+
+function initialize() {
+  const buttons = document.querySelectorAll('.button')
+
+  buttons.forEach(button => {
+    button.addEventListener('click', calculate)
+  });
+}
+
+let calcState = {};
+
+initialize();
